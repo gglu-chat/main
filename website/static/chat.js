dda304456336aff589e746b8a3b89b1fc31cf96b
bujijam <bujijam@qq.com>
2022-10-01 11:21:25 +0800

    better

--- website/static/chat.js
+++ website/static/chat.js
@@ -41,7 +41,12 @@ if (nick !== null && nick.match(/^[a-zA-Z0-9_]{1,12}$/)){
             var recvbox = document.createElement('div');
             recvbox.classList.add('info')
             var chatarea = document.getElementById('chatarea')
-            recvbox.appendChild(document.createTextNode('◆ 在线的用户：'+data.onlineUsers + ',' + nick));
+            if (data.onlineUsers.length == 0){
+                recvbox.appendChild(document.createTextNode('◆ 在线的用户：' + nick));
+            }
+            else{
+                recvbox.appendChild(document.createTextNode('◆ 在线的用户：'+data.onlineUsers + ',' + nick));
+            }    
             chatarea.insertBefore(recvbox, brick)
             socket.on('joinchat', function(dt){
                 if (dt.nick == nick){
