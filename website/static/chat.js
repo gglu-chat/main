f1bdd6ebe41e7af87087840064122c9737ee7e8c
bujijam <68989011+bujijam@users.noreply.github.com>
2022-10-07 11:39:25 +0800

    Update chat.js

--- website/static/chat.js
+++ website/static/chat.js
@@ -27,6 +27,7 @@ document.getElementById('colorscheme-selector').onchange = function(e){
 var trip = 'NOTRIP'
 var msg_id = 'MSG_ID'
 if (nick !== null && nick.match(/^[a-zA-Z0-9_]{1,12}$/)){
+    let nick, onlineUsers, password, trip;
     socket.on('connected', function(data){
         if (data.onlineUsers.indexOf(nick)===-1){
             socket.emit('join', {"type": "join", "nick": nick, "password": password});
