c784db8a048187508f05ddfc62bd93c1ad14783c
bujijam <bujijam@qq.com>
2023-01-05 10:48:06 +0800

    fix bug

--- website/main.py
+++ website/main.py
@@ -48,7 +48,7 @@ def getUserSid(nick):
     """获取指定用户名的sid"""
     for i in user_dict:
         if user_dict[i]['nick'] == nick:
-            user_sid = user_dict[i]
+            user_sid = i
     return user_sid
 
 @socketio.on('connect', namespace='/room')
@@ -133,11 +133,11 @@ def handle_message(arg):
     elif text[0] == '/':
         command = text.split(' ')[0]
         if command == '/kick' and level >= 3:
-            #try:
+            try:
                 target_nick = text.split(' ')[1]
                 disconnect(getUserSid(target_nick))
-            #except:
-                #sendInfo({"info": "请检查您的命令格式。"})
+            except:
+                sendInfo({"info": "请检查您的命令格式。"})
     # 字数超过750或者行数超过25行时折叠消息，否则正常发送
     elif len(text) >= 750 or text.count('\n') >= 25:
         emit('foldmsg', arg, to=room)
